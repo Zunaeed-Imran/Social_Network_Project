35298bac879820ea6a9a4d2784b6c3047993eb10
imran <zunaeede@gmail.com>
2024-06-04 14:07:56 +0600

    main - fix the border.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 <div>
   <form class="flex justify-center">
     <div class="space-y-12">
-      <div class="border-b border-gray-900/10 pb-12">
+      <div>
         <h2 class="text-base font-semibold leading-7 text-gray-900">Sing Up</h2>
       </div>
 
@@ -32,7 +32,7 @@
               <input id="email" name="password" type="password" autocomplete="password" class="block w-80 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
             </div>
           </div>
-          <div class="m-2 p-2">
+          <div class="m-2 p-2 border-b border-gray-900/10 pb-12">
             <button class="bg-blue-500 p-1 rounded">Submit</button>
           </div>
   </form>
@@ -41,7 +41,7 @@
 <div>
   <form class="flex justify-center">
     <div class="space-y-12">
-      <div class="border-b border-gray-900/10 pb-12">
+      <div>
         <h2 class="text-base font-semibold leading-7 text-gray-900">Sing In</h2>
       </div>
 
