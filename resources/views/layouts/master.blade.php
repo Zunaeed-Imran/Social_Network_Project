d387a9bd3cf9fbb68d341c3c6d22afbccbf3a543
imran <zunaeede@gmail.com>
2024-06-04 11:27:56 +0600

    main - add the tailwind setup in the project.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,12 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title')</title>
+  @vite('resources/css/app.css')
 </head>
 <body>
   @include('includes.header')
   <div class="container">
     @yield('content')
-    <h2>Hi</h2>
+    <h2 class="text-2xl">Hi</h2>
   </div>
 </body>
 </html>
